b99c9d6b2b6f2575e13a149b5c73d189aef89717
MatejMecka <matej.plavevski+github@gmail.com>
2017-12-16 18:29:12 +0100

    Handle translations

--- captionsTranslator.py
+++ captionsTranslator.py
@@ -5,6 +5,7 @@
 
 import argparse
 import srt
+import pysrt # This is bad practice forgive me. But each has it's cases i'm using it.
 import pydeepl
 import os
 import shutil
@@ -14,7 +15,7 @@ from datetime import datetime
 
 # Now this is where all the fun begins
 
-def translate(input, output, languagef, languaget):
+def translate(input, output, languagef, languagefnguaget):
 	file = open(input, 'r').read()
 	fileresp = open(output, 'w') # Use w mode instead
 	subs = list(srt.parse(file))
@@ -26,6 +27,31 @@ def translate(input, output, languagef, languaget):
 			fileresp.write("{}\n{} --> {}\n{}\n\n".format(sub.index,str(sub.start)[:-3], str(sub.end)[:-3], translationSentence))
 		except IndexError:
 			print("Error parsing data from deepl")
+
+	os.remove(input)		
+
+def handleTranslations(inp,out,laf,lat):
+	subs = pysrt.open(inp)
+	for sub in subs:
+		try:
+			# Check if it's a sentence if not check if there is another sentence there if not
+			sentence = None
+			if subs[sub.index].text.endswith('.') or subs[sub.index].text.endswith('?') or subs[sub.index].text.endswith('!'):
+				subs[sub.index].index - count
+			else:
+				subs[sub.index].text = subs[sub.index].text + '\n' + subs[sub.index+1].text
+				count+=1
+				print(count)
+				#subs[sub.index].index - count
+				subs[sub.index].end = subs[sub.index+1].end
+				del subs[sub.index+1]
+		except IndexError:		
+			pass
+
+	subs.save('translatedsubs.srt', encoding='utf-8')
+	translate('correcthorsebatterystaple.srt', out, laf, lat)		
+
+
 
 
 def parsefiles(inputFile, outputFile, languageFrom, languageTo):
@@ -40,8 +66,9 @@ def parsefiles(inputFile, outputFile, languageFrom, languageTo):
 		outputFile = inputFile + languageTo + '.srt'
 
 	shutil.copyfile(inputFile, outputFile)
+	shutil.copyfile(inputFile, 'correcthorsebatterystaple.srt') # So I don't overwrite the original file i'll create this temp one and then delete it. 
 	# Due to a bug that files cannot be accessed I had to move everything to another function
-	translate(inputFile, outputFile, languageFrom, languageTo)
+	handleTranslations(inputFile, outputFile, languageFrom, languageTo)
 
 
 def main():
@@ -56,3 +83,4 @@ def main():
 
 
 main()
+
