39bf43633bf22c5c226feda6eefcf087a734f3cf
Matej Plavevski <matej.plavevski@gmail.com>
2021-06-05 20:27:23 +0200

    Update captionsTranslator.py

--- captionsTranslator.py
+++ captionsTranslator.py
@@ -3,8 +3,6 @@
 
 import argparse
 import pysrt 
-#import pydeepl
-from deepl_scraper.translator import DeepLEngine
 import os
 import shutil
 import sys
@@ -14,27 +12,38 @@ import tempfile
 
 # Now this is where all the fun begins	
 
-def translate(input, output, languagef, languaget):
-	translator = DeepLEngine(source_language=languagef, target_language=languaget)
+def translate(input, output, languagef, languaget, api_key):
+
+	if not api_key:
+		from deepl_scraper.translator import DeepLEngine
+		translator = DeepLEngine(source_language=languagef, target_language=languaget)
+	else:
+		from deep_translator import DeepL
+
 	subs = pysrt.open(input)
 	fileresp = open(output, 'w') # Use w mode instead
 	for index, sub in enumerate(subs):
 		linefromsub = subs[index].text
 		try:
-			translationSentence = translator.translate(linefromsub)
+			if not api_key:
+				translationSentence = translator.translate(linefromsub)
+			else:
+				translationSentence = DeepL(api_key=api_key, source=languagef.lower(), target=languaget.lower()).translate(linefromsub)
 			print(str(sub.start) + ' ' + translationSentence)
 			fileresp.write("{}\n{} --> {}\n{}\n\n".format(sub.index,str(sub.start), str(sub.end), translationSentence))
 		except IndexError as e:
 			toSend = linefromsub.split('\n')
 			stringToWrite = ""
 			for part in toSend:
-				finString = stringToWrite + pydeepl.translate(part, languaget.upper(), languagef.upper()) + "\n"
+				if not api_key:
+					finString = stringToWrite + translator.translate(linefromsub)
+				else:
+					finString = stringToWrite + DeepL(api_key=api_key, source=languagef.lower(), target=languaget.lower()).translate(linefromsub)
 			fileresp.write("{}\n{} --> {}\n{}\n\n".format(sub.index,str(sub.start), str(sub.end), translationSentence))
-
 
 	os.remove(input)
 
-def handleTranslations(inp,out,laf,lat):
+def handleTranslations(inp,out,laf,lat, api_key):
 	subs = pysrt.open(inp)
 	append_index = None
 	remove_list = []                # List of unwanted indexes
@@ -59,10 +68,10 @@ def handleTranslations(inp,out,laf,lat):
 		subs[index].index = index + sub_index
 
 	subs.save(inp, encoding='utf-8')
-	translate(inp, out, laf, lat)
+	translate(inp, out, laf, lat, api_key)
 
 
-def parsefiles(inputFile, outputFile, languageFrom, languageTo):
+def parsefiles(inputFile, outputFile, languageFrom, languageTo, api_key):
 	if inputFile == None:
 		print("Input file not specified! Exiting...")
 		sys.exit(2)
@@ -73,12 +82,12 @@ def parsefiles(inputFile, outputFile, languageFrom, languageTo):
 	if outputFile == None:
 		outputFile = inputFile + languageTo + '.srt'
 
+
 	tempFile = tempfile.NamedTemporaryFile(suffix='.srt',delete=False)
 	shutil.copyfile(inputFile,tempFile.name)
 	shutil.copyfile(inputFile, outputFile) 
 	# Due to a bug that files cannot be accessed I had to move everything to another function
-	handleTranslations(tempFile.name, outputFile, languageFrom, languageTo)
-
+	handleTranslations(tempFile.name, outputFile, languageFrom, languageTo, api_key)
 
 def main():
 	# Parse Arguments
@@ -87,8 +96,9 @@ def main():
 	parser.add_argument('-o', '--output-file', action="store", help="takes the output file", metavar="FILE")
 	parser.add_argument('-lf', '--language-from', action="store", help="language to translate from", default="auto")
 	parser.add_argument('-lt', '--language-to', action="store", help="language to translate to")
+	parser.add_argument('-a', '--api-key', action="store", help="Deepl API Key - Optional")
 	args = parser.parse_args()
-	parsefiles(args.input_file, args.output_file, args.language_from, args.language_to)
+	parsefiles(args.input_file, args.output_file, args.language_from, args.language_to, args.api_key)
 
 
 main()
